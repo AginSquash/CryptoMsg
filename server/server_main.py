7ccbb79686c4adc3b3e1efb36571dced1cc530cf
AginS <you@example.com>
2019-01-25 23:37:31 +0300

    Added requestHandler and fix port error

--- server/server_main.py
+++ server/server_main.py
@@ -2,37 +2,41 @@ import socket
 import json
 import server_crypto
 import time
+import server_requestHandler
 
-sock = socket.socket()
-isNotConnected = True
+try:
+    sock = socket.socket()
+    isNotConnected = True
 
-while isNotConnected: 
-    try:
-        sock.bind(('', 4090))
-        isNotConnected = False
-        print("Connected")
-    except Exception as e:
-        print(str(e))
-        time.sleep(5)
+    while isNotConnected: 
+        try:
+            sock.bind(('', 4090))
+            isNotConnected = False
+            print("Connected")
+        except Exception as e:
+            print(str(e))
+            time.sleep(5)
 
-sock.listen(1)
+    sock.listen(1)
 
-while True:
-    try:
+    while True:
+        try:
 
-        conn, addr = sock.accept()
+            conn, addr = sock.accept()
 
-        print("connected: "+ str(addr))
-        data = conn.recv(1024)
-        json_fromClient = json.loads(data.decode()) #json.loads(data.decode())
-        print(json_fromClient["type"])
-        if (json_fromClient["type"]=="GetKey"):
-            server_crypto.CreateServerRSA()
-            #print(server_crypto.LoadServerPublicRSA(server_crypto._LoadServer_Private_RSA()))
-        if not data:
-            break
-        conn.send(("some key here!").encode()) #upper!
-        conn.close()
-    except Exception as e:
-        print(str(e))
-        break  
+            print("connected: "+ str(addr))
+            data = conn.recv(1024)
+            json_fromClient = json.loads(data.decode()) #json.loads(data.decode())
+            server_requestHandler.Handle(json_fromClient)    
+            if not data:
+                break
+            conn.send(("some key here!").encode()) #upper!
+            conn.close()
+        except Exception as e:
+            print(str(e))
+            conn.close()
+            print("Conn closed!")
+            break  
+except KeyboardInterrupt:
+    print("GoodBye!")
+    #conn.close()
---
+++ server/server_requestHandler.py
@@ -0,0 +1,5 @@
+def Handle(json_fromClient):
+    type_msg = json_fromClient["type"]
+    {
+        type_msg == "GetKey": print("GetKey request NEW")
+    }
